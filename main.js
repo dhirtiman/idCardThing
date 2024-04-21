66337d570ebec0c67e3d317aabcd6cb70a98d307
dhirtiman <dhirtimankhati@gmail.com>
2024-04-21 10:14:27 +0530

    working version does 10 per batch job

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const csv = require("csv-parser");
 const dataPoints = [];
 
 // Read data from CSV file
-
 async function writeDataOnPNG() {
   fs.createReadStream(csvFile)
     .pipe(csv())
@@ -14,83 +13,110 @@ async function writeDataOnPNG() {
       dataPoints.push(row);
     })
     .on("end", () => {
-      // Create canvas
+      let startIndex = 0;
+      let batchSize = 10;
+      let range = 1;
 
-      const canvas = createCanvas(imageDimensions.x, imageDimensions.y);
-      const ctx = canvas.getContext("2d");
+      function processBatch() {
+        if (startIndex < dataPoints.length) {
+          const canvas = createCanvas(imageDimensions.x, imageDimensions.y);
+          const ctx = canvas.getContext("2d");
 
-      // Load template background
-      loadImage(templateImage).then((image) => {
-        ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+          loadImage(templateImage).then((image) => {
+            ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
 
-        // Draw data points onto the canvas
-        ctx.textAlign = "center";
-        for (const rowData of dataPoints) {
-          ctx.font = "bold 96px serif"; // size for the STUDENT NAME
-          if (rowData["Student Name"].length > 16) {
-            // CONDITIONS if Name is  long
-            ctx.font = "bold 72px serif";
-            if (rowData["Student Name"].length > 21) {
-              ctx.font = "bold 69px serif";
-            }
-          }
+            const endIndex = Math.min(startIndex + batchSize, dataPoints.length);
+            const currentDataPoints = dataPoints.slice(startIndex, endIndex);
+            drawDataPoints(currentDataPoints, ctx, coordinates, range);
 
-          ctx.fillStyle = "#FFFF00"; // Yellow
-
-          let x = 800;
-          let y = 1440;
-          ctx.fillText(rowData["Student Name"], x, y); /// write student name
-
-          ctx.textAlign = "left";
-
-          ctx.font = "bold 56px serif"; // smaller text
-          if (rowData["Father's Name"].length > 16) {
-            // CONDITIONS if Name is  long
-            ctx.font = "bold 50px serif";
-            if (rowData["Father's Name"].length > 21) {
-              ctx.font = "bold 46px serif";
-            }
-          }
-
-          ctx.fillStyle = "#FFFFFF"; // back to white
-
-          ctx.fillText(rowData["Father's Name"], x - 180, y + 200); // write Father's Name
-
-          ctx.font = "bold 56px serif"; // smaller text
-          if (rowData["Mother's Name"].length > 16) {
-            // CONDITIONS if Name is  long
-            ctx.font = "bold 50px serif";
-            if (rowData["Mother's Name"].length > 21) {
-              ctx.font = "bold 40px serif";
-            }
-          }
-          ctx.fillText(rowData["Mother's Name"], x - 165, y + 316); // write Mother's Name
-
-
-          ctx.font = "bold 56px serif"; // smaller text
-          if (rowData.Address.length > 16) {
-            // CONDITIONS if Name is  long
-            ctx.font = "bold 50px serif";
-            if (rowData.Address.length > 21) {
-              ctx.font = "bold 40px serif";
-            }
-          }
-
-          ctx.fillText(rowData.Address, x - 300, y + 635);
-
-          //   ctx.fillText(rowData['Date of Birth'], x, y);
-          // ctx.fillText(rowData['Contact No.'], x - 140, y + 400);
-
-          break;
+            // Save image to disk
+            const out = fs.createWriteStream(
+              outputPath + `/${range}-${endIndex}.png`
+            );
+            const stream = canvas.createPNGStream();
+            stream.pipe(out);
+            out.on("finish", () => {
+              console.log(`Image ${range}-${endIndex} saved.`);
+              // Clear the stream before advancing to the next batch
+              stream.destroy();
+              // Update start index and range for the next batch
+              startIndex += batchSize;
+              range = endIndex;
+              // Process the next batch
+              processBatch();
+            });
+          });
         }
+      }
 
-        // Save image to disk
-        const out = fs.createWriteStream(outputPath + "/output.png");
-        const stream = canvas.createPNGStream();
-        stream.pipe(out);
-        out.on("finish", () => console.log("Image saved."));
-      });
+      // Start processing the first batch
+      processBatch();
     });
+}
+
+function drawDataPoints(dataPoints, ctx, coordinates, range) {
+  for (let i = 0; i < dataPoints.length; i++) {
+    if (i >= coordinates.length) break; // Ensure we don't exceed available coordinates
+    const { x, y } = coordinates[i];
+    writeOne(dataPoints[i], ctx, x, y);
+  }
+}
+
+async function writeOne(rowData, ctx, x, y) {
+  
+  ctx.textAlign = "center";
+  ctx.font = "bold 96px serif"; // size for the STUDENT NAME
+  if (rowData["Student Name"].length > 16) {
+    // CONDITIONS if Name is  long
+    ctx.font = "bold 72px serif";
+    if (rowData["Student Name"].length > 21) {
+      ctx.font = "bold 69px serif";
+    }
+  }
+
+  ctx.fillStyle = "#FFFF00"; // Yellow
+
+  ctx.fillText(rowData["Student Name"], x, y); /// write student name
+
+  ctx.textAlign = "left";
+
+  ctx.font = "bold 56px serif"; // smaller text
+  if (rowData["Father's Name"].length > 16) {
+    // CONDITIONS if Name is  long
+    ctx.font = "bold 50px serif";
+    if (rowData["Father's Name"].length > 21) {
+      ctx.font = "bold 46px serif";
+    }
+  }
+
+  ctx.fillStyle = "#FFFFFF"; // back to white
+
+  ctx.fillText(rowData["Father's Name"], x - 180, y + 200); // write Father's Name
+
+  ctx.font = "bold 56px serif"; // smaller text
+  if (rowData["Mother's Name"].length > 16) {
+    // CONDITIONS if Name is  long
+    ctx.font = "bold 50px serif";
+    if (rowData["Mother's Name"].length > 21) {
+      ctx.font = "bold 40px serif";
+    }
+  }
+  ctx.fillText(rowData["Mother's Name"], x - 120, y + 316); // write Mother's Name
+
+  ctx.font = "bold 56px serif"; // smaller text
+  if (rowData.Address.length > 16) {
+    // CONDITIONS if Name is  long
+    if (rowData.Address.length > 21) {
+      ctx.font = "bold 40px serif";
+    }
+  }
+  ctx.fillText(rowData.Address, x - 120, y + 635, 600); // write address
+
+  ctx.font = "bold 56px arial";
+
+  ctx.fillText(rowData["Contact No."], x - 120, y + 420); // write contact number
+
+  ctx.fillText(rowData["Date of Birth"], x - 120, y + 525);
 }
 
 const templateImage = "./templates/fullTemplate.png";
@@ -100,5 +126,18 @@ const imageDimensions = {
 };
 const outputPath = "./output/";
 const csvFile = "./data/class3CSV.csv";
+
+const coordinates = [
+  { x: 800, y: 1440 }, // 1st
+  { x: 2175, y: 1440 }, // 2nd
+  { x: 3530, y: 1440 }, // 3rd
+  { x: 4900, y: 1440 }, // 4th
+  { x: 6256, y: 1440 }, // 5th
+  { x: 800, y: 3800 },
+  { x: 2175, y: 3800 },
+  { x: 3530, y: 3800 },
+  { x: 4900, y: 3800 },
+  { x: 6256, y: 3800 },
+];
 
 writeDataOnPNG();
